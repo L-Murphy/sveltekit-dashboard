2f8d9b55162731071a8a384316c7e70b82d22b1d
L-Murphy <liammurph1998@gmail.com>
2025-12-15 11:47:23 -0500

    added init sideboard

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import { Sidebar } from '$lib/components';
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="container flex flex-row">
+	<Sidebar />
+	<main>
+		{@render children()}
+	</main>
+</div>
+
