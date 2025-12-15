50d968981463618369d647d69934d1eab96c7970
L-Murphy <liammurph1998@gmail.com>
2025-12-15 08:55:15 -0500

    added bits-ui and tailwind. created Card componenet anddashboard route

---
+++ frontend/src/routes/dashboard/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Card from "./Card.svelte";
+
+</script>
+
+<h1>This is the dashboard</h1>
+
+<div>
+    <h3>Sample box</h3>
+</div>
+
+<Card cardTitle="TFSA"/>
+<Card cardTitle="RRSP"/>
---
+++ frontend/src/routes/dashboard/Card.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    
+    let hidden = $state(true)
+    let props = $props();
+
+    const toggleHidden = () => {
+        hidden = !hidden
+    }
+</script>
+
+<div class="container flex">
+
+    <div class="">
+        <button onclick={toggleHidden}>{hidden ? "Show" : "Hide"}</button>
+    </div>
+    
+    <br>
+    
+    <div class={hidden ? "hidden" : ""} >
+        <h3>{props.cardTitle || "Untitled"}</h3>
+        <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+    </div>
+</div>
+    
+
+<style>
+    button{
+        color: red;
+        border: black;
+        border-radius: 1em;
+        background-color: bisque;
+        padding: 5px;
+    }
+
+    button:hover {
+        cursor: pointer;
+    }
+
+    .container{
+        border-width: 1px;
+        border-radius: 1em;
+        padding: .2em;
+        margin: .2em;
+    }   
+</style>
